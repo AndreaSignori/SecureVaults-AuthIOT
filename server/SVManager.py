567d2fd8e22ce8aa230c6a2b08f3b16f1632c2b8
denisG99 <gasparollodenis@gmail.com>
2025-02-02 14:01:09 +0100

    database table creation and some minor fix

--- server/SVManager.py
+++ server/SVManager.py
@@ -4,20 +4,33 @@ import sqlite3
 class SVManager:
     """
     This class provides an interface for managing SecureVaults stored in a database.
-    Our database is formed by a single table called `SecureVaults`, where each secure vault has associated an IoT device
+    Our database is formed by a single table called `devices`, where each secure vault has associated an IoT device
     since each one has a unique identifier.
     """
     def __init__(self, db_name: str):
         """
         Initialize new instance of the class.
 
-        :param db_name: database name
+        :param db_name: database name (the file name should be end without db)
         """
         if db_name is not None:
-            self._db_name : str = db_name if db_name.endswith(".db") else db_name + ".db"
+            self._db_name : str = db_name
+        #self._connection : sqlite3.Connection|None = None
+        #self.cursor : sqlite3.Cursor|None = None
+
+        # database creation if the table exist
+        with self._connect() as (conn, cur):
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS devices (
+                device_ID VARCHAR(30) PRIMARY KEY,
+                secure_vault TEXT DEFAULT NULL
+            )
+            """)
+            conn.commit()
+
 
     @contextlib.contextmanager
-    def connect(self) -> (sqlite3.Connection, sqlite3.Cursor):
+    def _connect(self) -> (sqlite3.Connection, sqlite3.Cursor):
         """
         Create a connection with the database based on a context manager
 
@@ -31,56 +44,51 @@ class SVManager:
         cur.close()
         conn.close()
 
-    def get_SV(self, cur: sqlite3.Cursor, id: str) -> tuple:
+    def get_SV(self, id: str) -> tuple:
         """
         Get the secure vault associated with the given ID.
 
-        :param cur: database cursor
         :param id: identifier for IoT device associated to a secure vault
 
         :return: secure vault
         """
-        return cur.execute(f"SELECT secure_vault FROM SecureVaults WHERE id=?", (id,)).fetchone()
+        with self._connect() as (_, cur):
+            return cur.execute(f"SELECT secure_vault FROM devices WHERE device_ID=?", (id,)).fetchone()
 
-    def insert_SV(self, conn: sqlite3.Connection, cur: sqlite3.Cursor, id: str, sv: str) -> None:
+    def insert_device(self, id: str) -> None:
         """
-        Insert new secure vault associated with the given ID.
+        Insert unregistered device into the database.
 
-        :param conn: database connection
-        :param cur: database cursor
         :param id: identifier for IoT device associated to a secure vault
-        :param sv: secure vault that we want to insert into the database
         """
-        if self._check_id_existence(id):
-            # TODO: check secure vault structure
-            cur.execute("INSERT INTO SecureVaults (sv) VALUES (?)", (sv,))
-            conn.commit()
+        if not self._check_id_existence(id):
+            with self._connect() as (conn, cur):
+                cur.execute("INSERT INTO devices (device_ID) VALUES (?)", (id,))
+                conn.commit()
         else:
-            print("ID not found!")
+            print(f"Device with ID {id} already registered!")
 
-    def update_SV(self, conn: sqlite3.Connection, cur: sqlite3.Cursor, id: str, sv: str) -> None:
+    def update_SV(self, id: str, sv: str) -> None:
         """
         Update an existing secure vault associated with the given ID.
 
-        :param conn: database connection
-        :param cur: database cursor
         :param id: identifier for IoT device associated to a secure vault
         :param sv: new value for secure vault
         """
         if self._check_id_existence(id):
-            # TODO: check secure vault structure
-            cur.execute("UPDATE SecureVaults SET sv=? WHERE id=?", (sv, id))
-            conn.commit()
+            with self._connect() as (conn, cur):
+                cur.execute("UPDATE devices SET secure_vault=? WHERE device_ID=?", (sv, id))
+                conn.commit()
         else:
-            print("ID not found!")
+            print(f"Device with ID {id} not found!")
 
-    def _check_id_existence(self, cur: sqlite3.Cursor, id: str) -> bool:
+    def _check_id_existence(self, id: str) -> bool:
         """
         Check if the given ID is already registered into the database, or not.
 
-        :param cur: database cursor
         :param id: identifier for IoT device which we want to check its existence
 
         :return: True if it exists, False otherwise
         """
-        return cur.execute("SELECT * FROM SecureVaults WHERE id=?", (id,)).fetchone() is not None
+        with self._connect() as (_, cur):
+            return cur.execute("SELECT * FROM devices WHERE device_ID=?", (id,)).fetchone() is not None
