f52e8df7106ceff9e2771566e6904a245b99492b
denisG99 <gasparollodenis@gmail.com>
2025-02-06 16:51:18 +0100

    code docs and comments for utils' functions

--- server/utils/utils.py
+++ server/utils/utils.py
@@ -1,18 +1,26 @@
 def padding(elem: str, target: int) -> str:
     """
-    Add in front of the bit sequence a series of zeros until it reaches target.
+    Add at the end of the bit sequence a series of zeros until it reaches target.
 
     :param elem: element where to apply padding
     :param target: dimension goal for the string
+
     :return: padded string
     """
-    diff = target - len(elem)
+    diff = target - len(elem) # number of elements need to add to reach the target dimension
 
     padding = '0' * diff
 
     return elem + padding
 
 def str_to_dict(s: str) -> dict:
+    """
+    Convert a string s that describe a dictionary into an actual dictionary.
+
+    :param s: string to convert into a dictionary
+
+    :return: dictionary described by string passed as parameter
+    """
     s = s.strip("{}").replace("\'", "" )
 
     return dict(item.split(": ") for item in s.split(", "))
