6af8a82a613b94a2b2bd26830172cd8550248eec
denisG99 <gasparollodenis@gmail.com>
2025-02-11 11:04:09 +0100

    ADD device memory manager

---
+++ client/memManager.py
@@ -0,0 +1,19 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+
+# CONFIG params
+KEY = b"super_secret_key"
+IV = b'0' * 16
+
+class memManager:
+    def __init__(self, mem_idx: str):
+        self._mem_idx = mem_idx
+
+    def read(self) -> str:
+        with open(self._mem_idx, "rb") as f:
+             return AES.new(KEY, AES.MODE_CBC, iv=IV).decrypt(f.read()).decode()
+
+    def write(self, new_content: str) -> None:
+        with open(self._mem_idx, "wb") as f:
+            f.write(AES.new(KEY, AES.MODE_CBC, iv=IV).encrypt(pad(new_content.encode(), AES.block_size)))
